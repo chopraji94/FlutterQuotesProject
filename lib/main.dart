0b5dfe21597a183f6bdb1a057e15ac13313580a2
chopraji94 <41725998+chopraji94@users.noreply.github.com>
2020-09-16 14:45:28 +0530

    Quotes With different File

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() => runApp(MaterialApp(
   home: QuotesList(),
@@ -11,10 +12,10 @@ class QuotesList extends StatefulWidget {
 
 class _QuotesListState extends State<QuotesList> {
 
-  List<String> quotes = [
-    'Be Yourself; everyone else is already taken',
-    'I a]have nothing to declare except my genius',
-    'The truth is rarely pure and never simple'
+  List<Quote> quotes = [
+    Quote(author: 'Wilferd', text: 'Be Yourself; everyone else is already taken'),
+    Quote(author: 'Harry', text: 'I have nothing to declare except my genius'),
+    Quote(author: 'Moron Hells', text: 'The truth is rarely pure and never simple'),
   ];
 
   @override
@@ -28,9 +29,12 @@ class _QuotesListState extends State<QuotesList> {
           'Awesome Quotes',
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: quotes.map((quote) => Text(quote)).toList(),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+        ),
       ),
     );
   }
